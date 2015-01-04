d064f1c2a36f45ffd5d8cb70d1e5ce64fb4c8315
Garrett Nay <garrettnay@ymail.com>
2015-01-03 23:36:54 -0700

    Remove unnecessary event handler

--- app/scripts/views/record-list.js
+++ app/scripts/views/record-list.js
@@ -3,8 +3,6 @@
 var View = require('ampersand-view');
 var template = require('../../templates/record-list.html');
 var ItemView = require('./thing-record-list-item');
-var NewThingView = require('./new-thing');
-var events = require('../events');
 
 module.exports = View.extend({
   template: template,
@@ -18,10 +16,6 @@ module.exports = View.extend({
 
   events: {
     'click [data-hook~=new-thing]': 'showNewThingForm'
-  },
-
-  showNewThingForm: function () {
-    events.trigger('modal:show', new NewThingView({collection: this.collection}));
   }
 
 });
