ae55740dc3e2d0657950df85141fba4d2c583fab
Garrett Nay <garrettnay@ymail.com>
2015-04-11 14:18:08 -0600

    Make localforage work with Webpack

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,10 @@
 'use strict';
 
+// Execute the dist version of localforage in the global context because it's
+// not compatible with Webpack yet. The module 'localforage' is aliased to the
+// global variable localforage, which is created by this script.
+require('script!localforage/dist/localforage.nopromises.js');
+
 var app = require('./app');
 
 document.addEventListener('DOMContentLoaded', function () {
