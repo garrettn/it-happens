e3e13ecb9f6a98a7b7b102354a44a393c2310eea
Garrett Nay <garrettnay@ymail.com>
2015-04-11 21:22:20 -0600

    Add files necessary for FFOS app

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,10 @@
 'use strict';
 
+// Copy the files necessary for Firefox OS
+require('!file?name=[name].[ext]!../manifest.webapp');
+require('!file?name=[name].[ext]!../images/icon128.png');
+require('!file?name=[name].[ext]!../images/icon512.png');
+
 // Execute the dist version of localforage in the global context because it's
 // not compatible with Webpack yet. The module 'localforage' is aliased to the
 // global variable localforage, which is created by this script.
